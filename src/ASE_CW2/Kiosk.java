09f548dba772de4fdb439ec0061f7844f12bac1a
asimoka <assemyerkinova@gmail.com>
2017-03-20 13:46:49 +0000

    number of passengers checked with available seat numbers

--- src/ASE_CW2/Kiosk.java
+++ src/ASE_CW2/Kiosk.java
@@ -12,41 +12,50 @@ public class Kiosk extends Thread {
 		this.PassengerGroupList=PassengerGroupList;
 	}
 	
-	/*public String getCombinedTaxiandPassengersStr(){
-		String output="";
-		if (TaxiList.isEmpty()) {
-			output="No available taxis at the moment";
-		} 
-		else if (PassengerGroupList.isEmpty()) {
-			output="No Passengers in the queue";
+	
+	
+	/*the method that returns the index of the taxi 
+	 * that has enough seats for the 
+	 * first passenger group in the queue*/
+	
+	public int taxiIndexWithEnoughSeats()
+	{
+		for (int i=0;i<=TaxiList.size();i++)
+		{
+			if (PassengerGroupList.get(0).getPassengersNumber()<=TaxiList.get(i).getMaxPassengersNumber()){return i;}
 		}
-		else{
-			output="Destination:  "+PassengerGroupList.get(0).getDestination()+"\nPassengers:  "+PassengerGroupList.get(0).getPassengersNumber()+
-					"\nTaxi No: "+TaxiList.get(0).getPlateNumber();
-			TaxiList.remove(0);
-			PassengerGroupList.remove(0);
-		}
-		return output;
-	}*/
+		return -1;
+	}
+	
+	/* the method that allocates passenger groups
+	 * with the taxi that is available and has
+	 * enough seats for this group*/ 
 	
 	public void run(){
 		String output="";
 		try {
-			while ((TaxiList.size()>0) && (PassengerGroupList.size()>0)){
-				output="\nDestination:  "+PassengerGroupList.get(0).getDestination()+"\nPassengers:  "+PassengerGroupList.get(0).getPassengersNumber()+
-						"\nTaxi No: "+TaxiList.get(0).getPlateNumber();
+			while ((TaxiList.size()>0) && (PassengerGroupList.size()>0))
+			{
+				if (taxiIndexWithEnoughSeats()!=-1)
+				{
+				output="\nDestination:  "+PassengerGroupList.get(0).getDestination()+"\nPassengers:  "
+						+PassengerGroupList.get(0).getPassengersNumber()+
+						"\nTaxi No: "+TaxiList.get(taxiIndexWithEnoughSeats()).getPlateNumber();
 				System.out.print(output);
-				TaxiList.remove(0);
+				TaxiList.remove(taxiIndexWithEnoughSeats());
 				PassengerGroupList.remove(0);
+				}
 			}
-		} catch (IndexOutOfBoundsException e) {
+		} 
+		catch (IndexOutOfBoundsException e) {
 			System.out.print("No enough parameters");
 		} 
 		if (TaxiList.isEmpty()) {
-			output="No available taxis at the moment";
+			output="\n\nNo available taxis at the moment.  \n"+PassengerGroupList.size()+" passenger groups left in the queue";
+			
 		} 
 		else if (PassengerGroupList.isEmpty()) {
-			output="No Passengers in the queue";
+			output="\n\nNo Passengers in the queue.  \n"+TaxiList.size()+" taxis are still available";
 		}
 		System.out.print(output);
 	}
